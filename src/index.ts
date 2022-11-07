953caef36ae2e979300adb4bba2962cadd956394
Yiyun Qin <yiyun.qin@mths.ca>
2022-11-07 21:29:43 +0000

    blowup numbers

--- src/index.ts
+++ src/index.ts
@@ -41,8 +41,8 @@ function blowup (string: string): string {
       } else {
         for (let i = 0; i < index; i++) {
           answer.push(indexNextString)
-          continue
         }
+        continue
       }
       // if the indexString is the last character in the string
     } else {
